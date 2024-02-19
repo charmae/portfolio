90fc8a01e6eba4bd9e44e15a1301987beb4127ab
Gladys Charmae Corsino <gctcorsino@gmail.com>
2024-02-19 09:32:28 +0800

    Add Recaptcha v3 in contact form

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,8 @@ const nextConfig = {
     env: {
         EMAILJS_KEY: process.env.EMAILJS_KEY, 
         EMAILJS_SERVICEID: process.env.EMAILJS_SERVICEID, 
-        EMAILJS_TEMPLATEID: process.env.EMAILJS_TEMPLATEID 
+        EMAILJS_TEMPLATEID: process.env.EMAILJS_TEMPLATEID,
+        EMAILJS_RECAPTCHA: process.env.EMAILJS_RECAPTCHA
     }
 };
 
