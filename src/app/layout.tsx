27cf755e373f6931ebe79c2323918cda8dfcc6f8
Gladys Charmae Corsino <gctcorsino@gmail.com>
2024-02-28 22:56:05 +0800

    Try plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>
-        {children}
-        </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@ import SkillSection from "./components/sections/skill";
 import About from "./components/sections/about";
 import ProjectSection from "./components/sections/projects";
 import ContactSection from "./components/sections/contact";
-
+import PlausibleProvider from "next-plausible";
 
 export default function Home() {
   return (
-    <>
-    <About/>
-    <SkillSection/>
-    <ProjectSection/>
-    <ContactSection/>
-    </>
+    <PlausibleProvider domain="gcharmae.com">
+      <About />
+      <SkillSection />
+      <ProjectSection />
+      <ContactSection />
+    </PlausibleProvider>
   );
 }
